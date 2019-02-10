700be170ca9452136eadba827819b1e7b1d66c01
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2019-02-11 00:06:52 +0530

    updated pm2 configs

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -8,6 +8,9 @@ module.exports = {
     },
     env_production: {
       NODE_ENV: "production",
+      UNOGS_API_1: "https://unogs-unogs-v1.p.rapidapi.com/api.cgi",
+      UNOGS_API_2: "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi",
+      API_KEY: "UzUrpeo6BYmshiu0Edkelfwq4Xx7p1GcrYNjsnF01LaDGsEs9T"
     }
   }]
 }
